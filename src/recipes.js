7e329e1e96512da678cd0228a0750074bd69a9d1
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-08 13:31:44 -0600

    recipe data

--- src/recipes.js
+++ src/recipes.js
@@ -1,1 +1,26 @@
-
+export const recipes = [
+  { 
+    name: 'spagetti',
+    instructions: 
+        { 
+          ingredients: 'noodles, sauce, mushrooms',
+          price: '$5',
+        } 
+  }, 
+  { 
+    name: 'green been casserole',
+    instructions: 
+        { 
+          ingredients: 'green beans, cream mushroom soup, crunchy onions',
+          price: '$10',
+        } 
+  }, 
+  { 
+    name: 'strawberry shortcake',
+    instructions: 
+        { 
+          ingredients: 'strawberries, sugar, shortcake',
+          price: '$10',
+        } 
+  }, 
+];
