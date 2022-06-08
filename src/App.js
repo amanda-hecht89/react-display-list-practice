56fe757d4165226cfed07761ca397bc4751d4b55
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-08 12:46:18 -0600

    finished pets

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 // import your arrays here
 import { candies } from './candies-data.js';
 import CandiesList from './CandiesList';
+import { pets } from './pets.js';
+import PetList from './PetList';
+
 
 function App() {
   return (
     <div className="App">
       <hr />
       <CandiesList candies={candies} />
+      <hr />
+      <PetList pets={pets} />
       <hr />
     </div>
   );
--- src/PetList.js
+++ src/PetList.js
@@ -1,1 +1,11 @@
+import PetItem from './PetItem';
 
+export default function PetList({ pets }) {
+  return (
+    <div className="pet-list">
+      {
+        pets.map((pet, i) => <PetItem {...pet} key={pet.name + i} />)
+      }
+    </div>
+  );
+}
