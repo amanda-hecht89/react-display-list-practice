8d790dcad260cd106670ae0c1cbb9d926219d641
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-08 15:49:40 -0600

    item and list function 4

--- src/Television/TelevisionItem.js
+++ src/Television/TelevisionItem.js
@@ -1,1 +1,8 @@
 
+export default function TelevisionItem({ name, genre, actors }) {
+  return <div>
+    {name}
+    {genre}
+    {actors}
+  </div>;
+}
--- src/Television/TelevisionList.js
+++ src/Television/TelevisionList.js
@@ -1,1 +1,9 @@
+import TelevisionItem from './TelevisionItem.js';
 
+export default function TelevisionList({ television }) {
+  return <div>
+    {
+      television.map((telelvision, i) => <TelevisionItem {...telelvision} key={telelvision + i} />)
+    }
+  </div>;
+}
